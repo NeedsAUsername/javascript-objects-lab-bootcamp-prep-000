67f6997249128ccac32dfd56cd39ee86d3d18d0d
quip550@gmail.com Chi <quip550@gmail.com>
2018-05-21 21:19:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var recipes = {
   apples : '2',
-}
+};
 
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObj = Object.assign({}, object)
-  newObj[key] = value
-  return newObj
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
 }
 
 // test function
-console.log(updateObjectWithKeyAndValue(recipes, 'orange', '3'))
-console.log(recipe)
+console.log(updateObjectWithKeyAndValue(recipes, 'orange', '3'));
+console.log(recipe);
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]= value;
+  return object;
+}
